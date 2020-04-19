2ae6e913a89cea050384e1381edab39a4546b605
Arvydas_Mickus <amickus@icloud.com>
2020-04-19 09:59:08 +0300

    Updates

---
+++ precache-manifest.4eb61852cfd73dae3321006dedcbacb6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58615aab8290a0c1917f01fa6fad635d",
+    "url": "/Catfish_react_final_project/index.html"
+  },
+  {
+    "revision": "35b88a063fc5a61540e4",
+    "url": "/Catfish_react_final_project/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "92a3a0898e0707176ce2",
+    "url": "/Catfish_react_final_project/static/css/main.15f03719.chunk.css"
+  },
+  {
+    "revision": "35b88a063fc5a61540e4",
+    "url": "/Catfish_react_final_project/static/js/2.fd9f6ca5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Catfish_react_final_project/static/js/2.fd9f6ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a3a0898e0707176ce2",
+    "url": "/Catfish_react_final_project/static/js/main.54aeb1b8.chunk.js"
+  },
+  {
+    "revision": "e2055b2a624857409c52",
+    "url": "/Catfish_react_final_project/static/js/runtime-main.c8b69a07.js"
+  },
+  {
+    "revision": "72592b80234cdc6abcc3a580f02b180c",
+    "url": "/Catfish_react_final_project/static/media/cardBcg.72592b80.jpg"
+  },
+  {
+    "revision": "4a88ac8a0bc961aea81df85b3cf6a4b2",
+    "url": "/Catfish_react_final_project/static/media/selector.4a88ac8a.jpg"
+  }
+]);
